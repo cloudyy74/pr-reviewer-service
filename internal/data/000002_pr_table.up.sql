c614c58ba9a8bb3a6ddd3736f1eecc8fd6d10a79
Vitas Remeykis <ccloudyy74@gmail.com>
2025-11-12 21:58:38 +0300

    feat: migrate in docker-compose

--- internal/data/000002_pr_table.up.sql
+++ internal/data/000002_pr_table.up.sql
@@ -3,6 +3,6 @@ create table if not exists pull_requests (
     title varchar(64) not null,
     author_id uuid not null references users(id) on delete cascade,
     status varchar(16) not null check (status in ('OPEN', 'MERGED')),
-    reviewers_ids uuid[] check (cardinality(reviewers_ids) <= 2)
+    reviewers_ids uuid[] check (cardinality(reviewers_ids) <= 2),
     need_more_reviewers boolean not null default false
 );
